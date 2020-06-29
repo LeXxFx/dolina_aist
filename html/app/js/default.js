774f9c673b4869d55a88ab51b22f1b10cc9a0901
akon <gvakon@gmail.com>
2020-06-29 11:26:27 +0300

    init

---
+++ html/app/js/default.js
@@ -0,0 +1,29 @@
+'use strict';
+var Home = function () {
+
+	var showcaseSlider = function () {
+		var sl = $('.sl');
+		if (sl.length) {
+			sl.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				arrows: true,
+				dots: true,
+				prevArrow: '<a class="slick-prev"><i class="fa fa-angle-left"></i></a>',
+				nextArrow: '<a class="slick-next"><i class="fa fa-angle-right"></i></a>',
+				adaptiveHeight: true
+			});
+		}
+	};
+
+	return {
+		init: function () {
+			showcaseSlider();
+		}
+	};
+}();
+
+$(document).ready(function ($) {
+	Home.init();
+});
